0e9f0f58ddb8c5e40b6f3aff6db72bff86f7bad9
thdgr <adam.bud182@gmail.com>
2024-10-30 10:40:56 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        'slide-in-from-top': {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-in-from-top-2': 'slide-in-from-top 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
